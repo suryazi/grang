494fd10d9bc6151f5aa3744581f29fef1184a7cb
Suryazi Samiullah Khan <git@reseller.suryazi.com>
2014-07-15 18:02:28 +0300

    Initial run after installing plugins

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,16 +5,18 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require jquery
+//= require jquery/dist/jquery
+//= require angular/angular
+//= require bootstrap/dist/js/bootstrap
 //= require_tree .
 //= require_self
 
 if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
-			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
-	})(jQuery);
+    (function($) {
+        $('#spinner').ajaxStart(function() {
+            $(this).fadeIn();
+        }).ajaxStop(function() {
+            $(this).fadeOut();
+        });
+    })(jQuery);
 }
