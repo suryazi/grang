fa2e65cbcde9b35377d71672c8d39123702095c8
Suryazi Samiullah Khan <git@reseller.suryazi.com>
2014-07-16 12:57:47 +0300

    Added Json files to Bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,96 @@
+import grails.converters.JSON
+
+import com.grang.Android
+import com.grang.Battery
+import com.grang.Camera
+import com.grang.Connectivity
+import com.grang.Display
+import com.grang.Hardware
+import com.grang.Phone
+import com.grang.PhoneDetail
+import com.grang.SizeAndWeight
+import com.grang.Storage
+
 class BootStrap {
 
     def init = { servletContext ->
+    	JSON.registerObjectMarshaller( Phone ) { Phone phone ->
+			return [
+					age : phone.age,
+					id : phone.phoneId,
+					imageUrl : phone.imageUrl,
+					name : phone.name,
+					snippet : phone.snippet
+			]
+		}
+		
+		JSON.registerObjectMarshaller( PhoneDetail ) { PhoneDetail phoneDetail ->
+			return [
+				additionalFeatures: phoneDetail.additionalFeatures,
+				android: phoneDetail.android,
+				availability: phoneDetail.availability,
+				battery: phoneDetail.battery,
+				camera: phoneDetail.camera,
+				connectivity: phoneDetail.connectivity,
+				description: phoneDetail.description,
+				display: phoneDetail.display,
+				hardware: phoneDetail.hardware,
+				id: phoneDetail.phoneId,
+				images: phoneDetail.images,
+				name: phoneDetail.name,
+				sizeAndWeight: phoneDetail.sizeAndWeight,
+				storage: phoneDetail.storage
+				]
+			}
+		
+		JSON.registerObjectMarshaller( Camera ) { Camera camera ->
+			return [
+					primary : camera.primaryCamera,
+					features : camera.features,
+			]
+		}
+		
+		File phonesFolder = grailsApplication.mainContext.getResource("/phonesJson").file
+		
+		phonesFolder.listFiles().each { File file ->
+			
+			if(file.name.equals("phones.json")){
+				
+				def jsonPhones = grails.converters.JSON.parse(file.text)
+				jsonPhones.each {
+					Phone phone = new Phone(it)
+					phone.phoneId = it.id
+					phone.save(flush:true, failOnError:true)
+				}
+				
+			} else {
+
+				def jsonPhoneDetail = grails.converters.JSON.parse(file.text)
+
+				PhoneDetail phoneDetail = new PhoneDetail(jsonPhoneDetail)
+				phoneDetail.phoneId = jsonPhoneDetail.id
+
+				phoneDetail.setAndroid(new Android(jsonPhoneDetail.android))
+				phoneDetail.setBattery(new Battery(jsonPhoneDetail.battery))
+				
+				Camera camera = new Camera(jsonPhoneDetail.camera)
+				camera.primaryCamera = jsonPhoneDetail.camera.primary
+				phoneDetail.setCamera(camera)
+				
+				phoneDetail.setConnectivity(new Connectivity(jsonPhoneDetail.connectivity))
+				phoneDetail.setDisplay(new Display(jsonPhoneDetail.display))
+				phoneDetail.setHardware(new Hardware(jsonPhoneDetail.hardware))
+				phoneDetail.setSizeAndWeight(new SizeAndWeight(jsonPhoneDetail.sizeAndWeight))
+				phoneDetail.setStorage(new Storage(jsonPhoneDetail.storage))
+
+				phoneDetail.save(flush:true, failOnError:true)
+
+			}
+		}
     }
+	
     def destroy = {
+		
     }
+	
 }
